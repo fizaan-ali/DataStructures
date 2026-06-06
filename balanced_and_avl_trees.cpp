4cff3c37b2679b742baf354340ecf9830b487c42
fizaan.mughal.01@gmail.com <fizaan.mughal.01@gmail.com>
2026-06-06 00:15:55 -0700

    avl

---
+++ balanced_and_avl_trees.cpp
@@ -0,0 +1,7 @@
+// balanced tree = height of left subtreee approx = height of right subtree
+// avl tree = self balancing bst
+
+// avl balanced factor = height(left subtree) - height(right subtree);
+// allowed values +1, 0, -1,
+// if none of these values it automatically balances by performing rotations
+// four types of rotations LL, RR, LR, RL
